e419b4c92cbfd15e2050152ae1d14814dc32e5b4
tario-you <tario01pd2024@gmail.com>
2024-10-12 14:11:53 -0700

    added nav

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+// components/Navbar.jsx
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css'; // Import the CSS file for styling
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li>
+          <NavLink to="/body-model" className="nav-link" activeClassName="active-link">
+            Body Model
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/level-up" className="nav-link" activeClassName="active-link">
+            Level Up
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/setup-form" className="nav-link" activeClassName="active-link">
+            Setup Form
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/worksouts" className="nav-link" activeClassName="active-link">
+            Workouts
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
