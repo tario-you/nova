8ed20e1073df5eb137f838088723c5dfccc5bb4b
tario-you <tario01pd2024@gmail.com>
2024-10-12 17:40:24 -0700

    hi

--- src/components/BodyModel/BodyModel.js
+++ src/components/BodyModel/BodyModel.js
@@ -5,84 +5,87 @@ import './BodyModel.css'; // This pulls the CSS file into your app
 const frontParts = [
   {
     name: 'abs',
-    image: 'body_parts/abs_full.png',
+    image: 'body_parts_model/abs_full.png',
     offsetX: -400,
     offsetY: -520,
     opacity: 0.1,
   },
   {
     name: 'upper abs',
-    image: 'body_parts/abs_upper.png',
+    image: 'body_parts_model/abs_upper.png',
     offsetX: -400,
     offsetY: -520,
     opacity: 0.1,
   },
   {
     name: 'lower abs',
-    image: 'body_parts/abs_lower.png',
+    image: 'body_parts_model/abs_lower.png',
     offsetX: -400,
     offsetY: -520,
     opacity: 0.1,
   },
   {
     name: 'biceps',
-    image: 'body_parts/bicep.png',
+    image: 'body_parts_model/bicep.png',
     offsetX: -400,
     offsetY: -605,
     opacity: 0.1,
   },
   {
     name: 'tibia',
-    image: 'body_parts/calves_front.png',
-    offsetX: -365,
+    image: 'body_parts_model/calves_front.png',
+    offsetX: -375,
     offsetY: 300,
+    opacity: 0.1,
   },
   {
     name: 'chest',
-    image: 'body_parts/chest.png',
+    image: 'body_parts_model/chest.png',
     offsetX: -400,
     offsetY: -600,
     opacity: 0.1,
   },
   {
     name: 'forearms',
-    image: 'body_parts/forearms_front.png',
-    offsetX: -400,
-    offsetY: 500,
+    image: 'body_parts_model/forearms_front.png',
+    offsetX: -100,
+    offsetY: -450,
+    rotation: 30,
+    scale: 0.8,
   },
   {
     name: 'obliques',
-    image: 'body_parts/obliques.png',
+    image: 'body_parts_model/obliques.png',
     offsetX: -400,
     offsetY: 500,
   },
   {
     name: 'shoulder_front',
-    image: 'body_parts/shoulder_front.png',
+    image: 'body_parts_model/shoulder_front.png',
     offsetX: -400,
     offsetY: 500,
   },
   {
     name: 'thigh_inner',
-    image: 'body_parts/thigh_inner.png',
+    image: 'body_parts_model/thigh_inner.png',
     offsetX: -400,
     offsetY: 500,
   },
   {
     name: 'thigh_lower',
-    image: 'body_parts/thigh_lower.png',
+    image: 'body_parts_model/thigh_lower.png',
     offsetX: -400,
     offsetY: 500,
   },
   {
     name: 'thigh_outer',
-    image: 'body_parts/thigh_outer.png',
+    image: 'body_parts_model/thigh_outer.png',
     offsetX: -400,
     offsetY: 500,
   },
   {
     name: 'thigh',
-    image: 'body_parts/thigh.png',
+    image: 'body_parts_model/thigh.png',
     offsetX: -400,
     offsetY: 500,
   },
@@ -91,43 +94,43 @@ const frontParts = [
 const backParts = [
   {
     name: 'back_lower',
-    image: 'body_parts/back_lower.png',
+    image: 'body_parts_model/back_lower.png',
   },
   {
     name: 'calves',
-    image: 'body_parts/calves.png',
+    image: 'body_parts_model/calves.png',
   },
   {
     name: 'forearms_back',
-    image: 'body_parts/forearms_back.png',
+    image: 'body_parts_model/forearms_back.png',
   },
   {
     name: 'glutes',
-    image: 'body_parts/glutes.png',
+    image: 'body_parts_model/glutes.png',
   },
   {
     name: 'hamstring',
-    image: 'body_parts/hamstring.png',
+    image: 'body_parts_model/hamstring.png',
   },
   {
     name: 'lats',
-    image: 'body_parts/lats.png',
+    image: 'body_parts_model/lats.png',
   },
   {
     name: 'neck',
-    image: 'body_parts/neck.png',
+    image: 'body_parts_model/neck.png',
   },
   {
     name: 'shoulder_back',
-    image: 'body_parts/shoulder_back.png',
+    image: 'body_parts_model/shoulder_back.png',
   },
   {
     name: 'trapezius',
-    image: 'body_parts/trapezius.png',
+    image: 'body_parts_model/trapezius.png',
   },
   {
     name: 'triceps',
-    image: 'body_parts/triceps.png',
+    image: 'body_parts_model/triceps.png',
   }
 ];
 
@@ -144,7 +147,8 @@ export default function App() {
             style={{
               position: 'absolute',
               left: `${part.offsetX || 0}px`,
-              top: `${part.offsetY || 0}px`
+              top: `${part.offsetY || 0}px`,
+              transform: `rotate(${part.rotation || 0}deg) scale(${part.scale || 1})`
             }}
           >
             <img src={part.image} alt={part.name} style={{ opacity: part.opacity || 0.5 }} />
